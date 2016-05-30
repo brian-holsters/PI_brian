54f7738154cc3682800ad57eced75c7eaabccd46
Brian <brian@intelligenia.com>
2016-05-30 13:41:15 +0200

    icono del selector de emote cambia al elegir un emote

--- src/PI_brian/static/js/post.js
+++ src/PI_brian/static/js/post.js
@@ -19,11 +19,14 @@ $(document).ready(function(){
     /*SELECCION DE EMOTE*/
 	var $botones_emote = $("a[data-emote_id]");
 	var $hidden_emote = $("#hidden_emote");
+	var $emote_selector_imagen = $("img.emote-selector-imagen");
 
 	$botones_emote.click(function(){
 		$this = $(this);
 		$hidden_emote.value = $this.data("emote_id");
-		console.log($hidden_emote);
+		var emote_src = $this.children("img.emote-selector-imagen").attr("src");
+		
+		$emote_selector_imagen.attr("src", emote_src);
     });
 
 	/*PROCESO AJAX*/
