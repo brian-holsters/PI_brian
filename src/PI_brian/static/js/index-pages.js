7038819510534b4c69baa7d875628899e7155110
Brian Holsters <brian.holsters@gmail.com>
2016-06-04 17:24:19 +0200

    formulario estilo bloque

--- src/PI_brian/static/js/index-pages.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var $pages = $("section[data-index-page]");
-    var height =  $(window).height() - $("#cabecera").height();
-    // La altura de cada sección se corresponde con lo que se ve en el total de la ventana.
-    $pages.css("height", height+"px");
-});
