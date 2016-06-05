6bebe574019a00432278157e451e07870fcead93
Brian Holsters <brian.holsters@gmail.com>
2016-06-05 16:38:23 +0200

    responder auto-focus

--- src/PI_brian/static/js/post/otro.js
+++ src/PI_brian/static/js/post/otro.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
 	/* BOTON RESPONDER*/
 	var $botones_responder = $("button[data-post-id]");
 	$botones_responder.click(function(){
-		$("div.responder-container[data-post-id=" + $(this).data("post-id") + "]").toggleClass("hidden");
+	    $target = $("div.responder-container[data-post-id=" + $(this).data("post-id") + "]")
+		$target.toggleClass("hidden");
+		console.log($target.children().children(".respuesta-texto"));
+		$target.children().children(".respuesta-texto").focus();
 	});
 
 	/*RESPONDER A UN POST*/
